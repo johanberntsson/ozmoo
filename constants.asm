3eb039dd102259c0e56b768a4ff2e9883f7a65c0
Johan Berntsson <johanberntsson@users.noreply.github.com>
2018-04-28 21:38:08 +1000

    Added some debugging tools

--- constants.asm
+++ constants.asm
@@ -1,8 +1,15 @@
-;; Start of BASIC program
+; Start of BASIC program
 basic = $0801
 
-;; Background color
-bgcol = $d021
+; I/O registers
+background_color = $d021 
+border_color     = $d020
 
-;; Border color
-bocol = $d020
+; BASIC rom routines
+print_string     = $ab1e ; write string in A/Y (LO <, HI >)
+print_integer    = $bdcd ; write integer value in A/X
+
+; Kernel routines
+print_char       = $FFD2 ; write char in <a>
+setcursor        = $e50c ; set cursor to x/y (row/column)
+
--- ozmoo.asm
+++ ozmoo.asm
@@ -1,34 +1,64 @@
+; Define DEBUG for additional runtime printouts
+DEBUG = 1
+
+; where to story data
+mem_start = $2000
+
+; story file header constants
+header_version = $1a
+header_filelength = $1a
+
+; basic program (10 SYS2061)
 !source "basic-boot.asm"
+    +start_at $080d
+    jmp .initialize
 
-+start_at $080d
-
-    jmp .initialise
-
+; global variables
 err !byte 0
-
+; include other assembly files
 !source "memory.asm"
 
-.initialise
-    ; read the header
-    lda #$20    ; start in $2000
-    ldx #$00    ; first block to read
-    ldy #$01    ; read 1 sector
-    jsr readblocks
+; ---- to be moved into a debug or screen library later on ----
+.printstring
+    ; subroutine: print string
+    lda #<.msg
+    ldy #>.msg
+    jmp print_string
 
-    ; check file length
-    lda $201A
-    ASL
-    STA err
-    INC err
+.msg !pet "total blocks: ",0
 
-    ldx err
+.printx
+    ; subroutine: print value stored in x register
     LDA #$00
-    JSR $BDCD      ; write counter
+    JSR print_integer
+    LDA #13
+    JMP print_char
 
-    ; read the rest
-    lda #$21    ; start in $2100
-    ldx #$01    ; first block to read
-    ldy err    ; read 16 sectors
+; ----- the main routine ----
+
+.initialize
+    ; read the header
+    lda #>mem_start ; first free memory block
+    ldx #$00    ; first block to read from floppy
+    ldy #$01    ; read 1 block
     jsr readblocks
 
+    ; check file length (need to be doubled)
+    lda mem_start + header_filelength
+    ASL          ; *2
+    STA err      ; store length
+
+!ifdef DEBUG {
+    ; show how many blocks to read (exluding header)
+    jsr .printstring
+    ldx err
+    jsr .printx
+}
+    ; read the rest
+    ldx #>mem_start ; first free memory block
+    inx        ; skip header
+    txa
+    ldx #$01    ; first block to read from floppy
+    ldy err    ; read <header_filelength> blocks
+    jsr readblocks
     rts
