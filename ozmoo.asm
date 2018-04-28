1fc01c87ec64378cabdb4aa787a8799f6dc22e44
Johan Berntsson <johanberntsson@users.noreply.github.com>
2018-04-29 08:05:09 +1000

    Added screen.asm

--- ozmoo.asm
+++ ozmoo.asm
@@ -17,56 +17,7 @@ header_filelength = $1a
 err !byte 0
 ; include other assembly files
 !source "memory.asm"
-
-; ---- to be moved into a debug or screen library later on ----
-.print2
-    ; trying message passing with implicit argument(s)
-    ; usage:
-    ;    jsr .print2
-    ;    !text "message",0
-    ; is supposed to use stack pointer to print the message until 0,
-    ; then update the stack so that RTS return to next instruction
-    ; after the text
-    ;
-    PLA     ; remove LO for return address
-    STA .return_address + 1;
-    PLA     ; remove HI for return address
-    STA .return_address;
-
--   LDA .return_address
-    INC .return_address + 1
-    BNE +
-    INC .return_address
-+   JSR print_char
-    BNE -
-
-    LDA .return_address
-    PHA 
-    LDA .return_address + 1
-    PHA
-    RTS
-
-.return_address !word $0000
-
-.printstring
-    ;jsr .print2
-    ;!pet "hello:",0
-
-    ; subroutine: print string
-    lda #<.msg
-    ldy #>.msg
-    jmp print_string
-
-.msg !pet "total blocks: ",0
-
-.printx
-    ; subroutine: print value stored in x register
-    LDA #$00
-    JSR print_integer
-    LDA #13
-    JMP print_char
-
-; ----- the main routine ----
+!source "screen.asm"
 
 .initialize
     ; read the header
@@ -82,9 +33,10 @@ err !byte 0
 
 !ifdef DEBUG {
     ; show how many blocks to read (exluding header)
-    jsr .printstring
+    jsr printstring
+    !pet "total blocks: ",0
     ldx err
-    jsr .printx
+    jsr printx
 }
     ; read the rest
     ldx #>mem_start ; first free memory block
---
+++ screen.asm
@@ -0,0 +1,45 @@
+; screen update routines
+; - printx
+; - printstring
+
+printx
+    ; subroutine: print value stored in x register
+    LDA #$00
+    JSR print_integer
+    LDA #13
+    JMP print_char
+
+printstring
+    ; print text (implicit argument passing)
+    ; usage:
+    ;    jsr .print2
+    ;    !text "message",0
+    ; uses stack pointer to find start of text, then
+    ; updates the stack so that execution continues
+    ; directly after the end of the text
+
+    ; store the return address
+    ; return address start -1 before the first character
+    PLA  ; remove LO for return address
+    STA .return_address + 1; 13
+    PLA  ; remove HI for return address
+    STA .return_address + 2; 09
+
+    ; print the string
+-   INC .return_address + 1
+    BCC .return_address
+    INC .return_address + 2
+.return_address
+    LDA $0000 ; self-modifying code (aaarg! but oh, so efficent)
+    BEQ +
+    JSR print_char
+    JMP -
+
+    ; put updated return address on stack
++   LDA .return_address + 2
+    PHA 
+    LDA .return_address + 1
+    PHA
+    RTS
+
+
