5b699fc2d17a6c7d665cb32af63f9a8e11a743d8
Johan Berntsson <johanberntsson@users.noreply.github.com>
2018-05-06 17:22:54 +1000

    Added support for Z3,Z4 and Z8 in convert_from_paddr

--- text.asm
+++ text.asm
@@ -2,19 +2,32 @@
 ;
 ; DragonTroll: PRINT_PADDR S030 ("The Dragon and the Troll"): 8d 03 1b
 
-convert_paddr
+convert_from_paddr
     ; convert a/x to paddr in .addr
     stx .addr + 2 ; 1b
     sta .addr + 1 ; 03
     lda #$0
     sta .addr
 
-    asl .addr+2
+!ifdef Z3 {
+    ldx #1
+}
+!ifdef Z4 {
+    ldx #2
+}
+!ifdef Z5 {
+    ldx #2
+}
+!ifdef Z8 {
+    ldx #3
+}
+
+-   asl .addr+2
     rol .addr+1
     rol .addr
-    asl .addr+2
-    rol .addr+1
-    rol .addr
+    dex
+    bne -
+
     ; $031b -> $00, $0c, $6c
     rts
 
@@ -30,26 +43,27 @@ read_text_byte
     inc .addr
 +   rts
 
-print_paddr
--   jsr read_text_byte
+print_addr
+    jsr read_text_byte
     sta .packedtext
     jsr read_text_byte
     sta .packedtext + 1
 
     ; extract 3 zchars (5 bits each)
     ldx #0
---  lda .packedtext + 1
+.extract_loop
+  lda .packedtext + 1
     and #$1f
     sta .zchars,x
 
     ldy #5
---- lsr .packedtext
+-   lsr .packedtext
     ror .packedtext+1
     dey
-    bne ---
+    bne -
     inx
     cpx #3
-    bne --
+    bne .extract_loop
 
     ; print the three chars
     ldx #2
@@ -91,7 +105,7 @@ print_paddr
     bpl --
 
     lda .packedtext + 1
-    beq -
+    beq print_addr
     rts
 
 testtext
@@ -99,11 +113,11 @@ testtext
     sta .alphabet_offset
     lda #$03
     ldx #$1b
-    jsr convert_paddr
+    jsr convert_from_paddr
     ; $0c6c: 13 2d 28 04 26 e6
     ; $132d: 0 00100 11001 01101 = 4 25 13: (A1) Th
     ; $2804: 0 01010 00000 00100 =
-    jsr print_paddr
+    jsr print_addr
     rts
 
 .addr !byte 0,0,0
