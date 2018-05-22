b87a732fdc0630eeb0f6679f707c77e9402fa6a5
Johan Berntsson <johanberntsson@users.noreply.github.com>
2018-05-22 15:08:48 +1000

    Z3 detection of score and time games added

--- screen.asm
+++ screen.asm
@@ -1,48 +1,8 @@
 ; screen update routines
 ; TRACE_WINDOW = 1
 
-current_text_style !byte 0 ; always 0 (roman) for Z3
-
 .current_window !byte 0
 .cursor_position !byte 0,0
-
-draw_status_line
-    jsr save_cursor
-    ldx #0
-    ldy #0
-    jsr set_cursor
-    lda #18 ; reverse on
-    jsr kernel_printchar
-    ; name of the object whose number is in the first global variable
-    ldy #0
-    lda z_global_vars_start,y ; 22
-    sta object_tree_ptr 
-    iny 
-    ldx z_global_vars_start,y ; 54
-    stx object_tree_ptr + 1
-!ifndef Z4PLUS {
-    ldy #8
-}
-!ifdef Z4PLUS {
-    ldy #13
-}
-    lda (object_tree_ptr),y ; low byte
-    tax
-    dey
-    lda (object_tree_ptr),y ; high byte
-    jsr set_z_address
-    jsr read_next_byte ; length of object short name
-    jsr print_addr
-
--   lda zp_screencolumn
-    cmp #40
-    beq +
-    lda #$20
-    jsr kernel_printchar
-    jmp -
-+   lda #146 ; reverse off
-    jsr kernel_printchar
-    jmp restore_cursor
 
 set_cursor
     ; input: y=column (0-39)
@@ -61,6 +21,88 @@ restore_cursor
     ldx .cursor_position
     ldy .cursor_position + 1
     jmp set_cursor
+
+
+!ifdef Z3 {
+draw_status_line
+    jsr save_cursor
+    ldx #0
+    ldy #0
+    jsr set_cursor
+    lda #18 ; reverse on
+    jsr kernel_printchar
+    ;
+    ; Room name
+    ; 
+    ; name of the object whose number is in the first global variable
+    ldy #0
+    lda z_global_vars_start,y ; 22
+    sta object_tree_ptr 
+    iny 
+    ldx z_global_vars_start,y ; 54
+    stx object_tree_ptr + 1
+    ldy #8
+    lda (object_tree_ptr),y ; low byte
+    tax
+    dey
+    lda (object_tree_ptr),y ; high byte
+    jsr set_z_address
+    jsr read_next_byte ; length of object short name
+    jsr print_addr
+    ;
+    ; fill the rest of the line with spaces
+    ;
+-   lda zp_screencolumn
+    cmp #40
+    beq +
+    lda #$20
+    jsr kernel_printchar
+    jmp -
+    ;
+    ; score or time game?
+    ;
++   lda story_start + header_flags_1
+    and #$40
+    bne .timegame
+    ; score game
+    ldx #0
+    ldy #20
+    jsr set_cursor
+    ldy #0
+-   lda .score_str,y
+    beq +
+    jsr kernel_printchar
+    iny
+    bne -
++   ldy #30
+    jsr set_cursor
+    ldy #0
+-   lda .moves_str,y
+    beq +
+    jsr kernel_printchar
+    iny
+    bne -
++   jmp .statusline_done
+.timegame
+    ; time game
+    ldx #0
+    ldy #20
+    jsr set_cursor
+    ldy #0
+-   lda .time_str,y
+    beq +
+    jsr kernel_printchar
+    iny
+    bne -
++
+.statusline_done
+    lda #146 ; reverse off
+    jsr kernel_printchar
+    jmp restore_cursor
+}
+.score_str !pet "Score: ",0
+.moves_str !pet "Moves: ",0
+.time_str !pet "Time: ",0
 
 z_ins_split_window
     ; split_window lines
@@ -103,7 +145,6 @@ z_ins_set_text_style
     jsr newline
 }
     lda z_operand_value_low_arr
-    sta current_text_style
     bne .t0
     ; roman
     lda #146 ; reverse off
