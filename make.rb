bb6387bdb773269d9dec68a1ede540e48e26c560
fredrikramsberg <fredrik.ramsberg@ibinder.com>
2020-10-28 23:55:10 +0100

    Speed up decrunching on Plus4.

--- make.rb
+++ make.rb
@@ -718,19 +718,21 @@ def build_specific_boot_file(vmem_preload_blocks, vmem_contents)
 		File.size($compmem_filename)].min}"
 
 	font_clause = ""
+	asm_clause = ""
 	if $font_filename
 		font_clause = " \"#{$font_filename}\"@#{$font_address}"
 	end
 	exo_target = ""
 	if $target == 'plus4'
 		exo_target = " -t4"
+		asm_clause = "-s \"lda $ff06 and \#\$ef sta $ff06\" -f \"lda $ff06 ora \#\$10 sta $ff06\""
 	end
 	if $target == 'c128'
 		exo_target = " -t128"
 	end
 #	exomizer_cmd = "#{$EXOMIZER} sfx basic -B -X \'LDA $D012 STA $D020 STA $D418\' ozmoo #{$compmem_filename},#{$storystart} -o ozmoo_zip"
 #	exomizer_cmd = "#{$EXOMIZER} sfx #{$start_address} -B -M256 -C -x1 #{font_clause} \"#{$ozmoo_file}\"#{compmem_clause} -o \"#{$zip_file}\""
-	exomizer_cmd = "#{$EXOMIZER} sfx #{$start_address}#{exo_target} -B -M256 -C #{font_clause} \"#{$ozmoo_file}\"#{compmem_clause} -o \"#{$zip_file}\""
+	exomizer_cmd = "#{$EXOMIZER} sfx #{$start_address}#{exo_target} -B -M256 -C #{font_clause} #{asm_clause} \"#{$ozmoo_file}\"#{compmem_clause} -o \"#{$zip_file}\""
 
 	puts exomizer_cmd
 	ret = system(exomizer_cmd)
