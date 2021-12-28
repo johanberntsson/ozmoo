3ff7e1e468464e5de21014dec4f926a1d84b94d0
Fredrik Ramsberg <fredrik.ramsberg@gmail.com>
2021-12-28 11:21:50 +0100

    Fixed a recently introduced bug where a custom alphabet table could replace character 7 in A2 (newline).

--- asm/text.asm
+++ asm/text.asm
@@ -2089,10 +2089,10 @@ print_addr
 	bne .not_A2
 ; newline?
 	cmp #7
-;	bne .l0b
-;	lda #13
-;	jmp .print_normal_char ; Always jump
-;.l0b 
+	bne .l0b
+	lda #13
+	jmp .print_normal_char ; Always jump
+.l0b 
 	; Direct jump for all normal chars in A2
 	bcs .l6
 	; escape char?
