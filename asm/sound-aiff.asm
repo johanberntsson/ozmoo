ee021481ea3200e4c7348dcca7bea9c32d35f001
Johan Berntsson <johan.mayumi@gmail.com>
2022-01-24 17:01:34 +0900

    Refactored AIFF code

---
+++ asm/sound-aiff.asm
@@ -0,0 +1,176 @@
+.parse_aiff
+    ; parses AIFF at $4000
+    ;
+    ; simplifying assumptions:
+    ; - only one SSND chunk
+    ; - no comments in the SSND chunk
+    ; - max size of each chunk other chunk is 256 bytes
+
+    ; parse the AIFF header
+    jsr .init_fastRAM_base
+    ; skip main header
+    lda #12
+    jsr .add_fastRAM_base
+
+.check_chunk
+    ; check what kind of chunk this is
+    ldz #2
+    lda [sound_file_target],z
+    pha
+    lda #4
+    jsr .add_fastRAM_base ; skip chunk identifier (4 bytes)
+    pla
+    cmp #$4e ; is it ssNd?
+    bne +
+    jmp .ssnd_chunk
++
+    ; COMM, FORM, INST, MARK or SKIP chunk, skip this
+    cmp #$4d ; is it coMm?
+    bne .skip_chunk
+    ; COMM chunk
+    ; check channels (expect 1)
+    ldz #5
+    lda [sound_file_target],z
+    cmp #1
+    bne .bad_format
+    ; check bits/sample (expect 8 bit)
+    ldz #11
+    lda [sound_file_target],z
+    cmp #8
+    bne .bad_format
+    ; extract the sampling rate
+    ; exponent (= (byte 12 and 13) - $3fff, only positive allowed)
+    ldz #12
+    lda [sound_file_target],z
+    sec
+    sbc #$3f
+    sta .exponent + 1
+    inz 
+    lda [sound_file_target],z
+    sbc #$ff
+    sta .exponent
+    ; only use one byte of fraction (enough precision as int)
+    inz
+    lda [sound_file_target],z
+    sta .sample_rate_big_endian + 1
+    inz
+    lda [sound_file_target],z
+    sta .sample_rate_big_endian + 2
+    lda #0
+    sta .sample_rate_big_endian
+    ; modfiy with exponent
+    lda .exponent
+    sec
+    sbc #7 ; we're shifting one byte
+    tax
+-   clc
+    rol .sample_rate_big_endian + 2
+    rol .sample_rate_big_endian + 1
+    rol .sample_rate_big_endian
+    dex
+    bne -
+!ifdef TRACE_SOUND {
+    lda .sample_rate_big_endian
+    jsr print_byte_as_hex
+    jsr colon
+    lda .sample_rate_big_endian + 1
+    jsr print_byte_as_hex 
+    jsr newline
+}
+    jmp .skip_chunk
+
+.bad_format
+    rts
+
+.skip_chunk
+    ldz #3
+    lda [sound_file_target],z
+    jsr .add_fastRAM_base ; skip chunk data (TODO check full size?)
+    lda #4
+    jsr .add_fastRAM_base ; skip chunk length (4 bytes)
+    jmp .check_chunk
+
+.ssnd_chunk
+    ; is the sample too big?
+    ldz #1
+    lda [sound_file_target],z
+    bne .bad_format
+
+    ; save chunk size for later
+    inz
+-   lda [sound_file_target],z
+    pha
+    inz
+    cpz #4
+    bne -
+
+    lda #12
+    jsr .add_fastRAM_base ; skip until sample data (assuming no comment)
+    ; stop playback while loading new sample data
+    lda #$00
+    sta $d720
+    ; load sample address into base and current address
+    lda sound_file_target 
+    sta sample_start_address
+    lda sound_file_target + 1
+    sta sample_start_address + 1
+    lda sound_file_target + 2
+    sta sample_start_address + 2
+    ; calculate end point by adding saved chunk size to sample start
+    ; todo: adjust for comment and chunk header (-8)
+    clc
+    pla
+    adc sample_start_address
+    sta sample_stop_address
+    pla
+    adc sample_start_address + 1
+    sta sample_stop_address + 1
+
+    ; frequency (assuming CPU running at 40 MHz)
+    ; x = (f * 40)/100 = (f/10) << 2
+    lda .sample_rate_big_endian + 1 ; note big endian
+    sta dividend
+    lda .sample_rate_big_endian
+    sta dividend + 1
+    lda #10
+    sta divisor
+    lda #$00
+    sta divisor + 1
+    jsr divide16
+    clc
+    rol dividend
+    rol dividend + 1
+    rol dividend
+    rol dividend + 1
+    lda dividend
+    sta sample_frequency
+    lda dividend + 1
+    sta sample_frequency + 1
+    lda #$00
+    sta sample_frequency + 2
+    rts
+
+.init_fastRAM_base
+    ; init sound file address pointer
+	lda #0
+	sta sound_file_target
+	sta sound_file_target + 1
+	sta sound_file_target + 3
+	lda #$04
+	sta sound_file_target + 2
+	rts
+
+.add_fastRAM_base
+    ; add (a) to sound file address pointer
+    clc
+    adc sound_file_target
+    sta sound_file_target
+    lda sound_file_target + 1
+    adc #$00
+    sta sound_file_target + 1
+    rts
+
+.exponent !byte 0,0
+.sample_rate_big_endian !byte 0, 0, 0 ; two first bytes are value in Hz
+
+
--- asm/sound.asm
+++ asm/sound.asm
@@ -29,6 +29,7 @@
 !ifdef TARGET_MEGA65 {
 
 ;TRACE_SOUND = 1
+SOUND_AIFF_ENABLED = 1
 
 sound_load_msg !pet "Loading sound: ",0
 sound_load_msg_2 !pet 13,"Done.",0
@@ -255,6 +256,13 @@ sound_arg_volume !byte 0
 sound_arg_repeats !byte 0
 sound_arg_routine !byte 0, 0
 
+sample_start_address !byte 0,0,0
+sample_stop_address !byte 0,0,0
+sample_frequency !byte 0,0,0
+
+.current_effect !byte $ff
+.bad_audio_format_msg !pet "[unsupported audio format]", 13, 0
+
 sound_effect
     ; currently we ignore 1 prepare and 4 finish with
     lda sound_arg_effect
@@ -291,171 +299,50 @@ sound_effect
     ; load sound effect into fastRAM at $40000
     stx .current_effect ; store as zero indexed index instead
     jsr .copy_effect_to_fastram
-+   ; play sound effect
-    jmp .play_aiff
-
-.exponent !byte 0,0
-.sample_rate_big_endian !byte 0, 0, 0 ; two first bytes are value in Hz
-.bad_audio_format_msg !pet "[bad audio format]", 13, 0
-.current_effect !byte $ff
-
-.play_aiff
-    ; plays AIFF at $4000
-    ;
-    ; simplifying assumptions:
-    ; - only one SSND chunk
-    ; - no comments in the SSND chunk
-    ; - max size of each chunk other chunk is 256 bytes
-
-    ; parse the AIFF header
-    jsr .init_fastRAM_base
-    ; skip main header
-    lda #12
-    jsr .add_fastRAM_base
-
-.check_chunk
-    ; check what kind of chunk this is
-    ldz #2
-    lda [sound_file_target],z
-    pha
-    lda #4
-    jsr .add_fastRAM_base ; skip chunk identifier (4 bytes)
-    pla
-    cmp #$4e ; is it ssNd?
-    bne +
-    jmp .ssnd_chunk
-+
-    ; COMM, FORM, INST, MARK or SKIP chunk, skip this
-    cmp #$4d ; is it coMm?
-    bne .skip_chunk
-    ; COMM chunk
-    ; check channels (expect 1)
-    ldz #5
-    lda [sound_file_target],z
-    cmp #1
-    bne .bad_format
-    ; check bits/sample (expect 8 bit)
-    ldz #11
-    lda [sound_file_target],z
-    cmp #8
-    bne .bad_format
-    ; extract the sampling rate
-    ; exponent (= (byte 12 and 13) - $3fff, only positive allowed)
-    ldz #12
-    lda [sound_file_target],z
-    sec
-    sbc #$3f
-    sta .exponent + 1
-    inz 
-    lda [sound_file_target],z
-    sbc #$ff
-    sta .exponent
-    ; only use one byte of fraction (enough precision as int)
-    inz
-    lda [sound_file_target],z
-    sta .sample_rate_big_endian + 1
-    inz
-    lda [sound_file_target],z
-    sta .sample_rate_big_endian + 2
-    lda #0
-    sta .sample_rate_big_endian
-    ; modfiy with exponent
-    lda .exponent
-    sec
-    sbc #7 ; we're shifting one byte
-    tax
--   clc
-    rol .sample_rate_big_endian + 2
-    rol .sample_rate_big_endian + 1
-    rol .sample_rate_big_endian
-    dex
-    bne -
-!ifdef TRACE_SOUND {
-    lda .sample_rate_big_endian
-    jsr print_byte_as_hex
-    jsr colon
-    lda .sample_rate_big_endian + 1
-    jsr print_byte_as_hex 
-    jsr newline
++   ; parse sound effect data
+    lda #$00
+    sta sample_start_address + 2;
+!ifdef SOUND_AIFF_ENABLED {
+    jsr .parse_aiff
 }
-    jmp .skip_chunk
+    ; error if sample_start_address not set
+    lda sample_start_address + 2;
+    bne +
+    lda #>.bad_audio_format_msg
+    ldx #<.bad_audio_format_msg
+    jmp printstring_raw
++   ; play the sample
+    jmp .play_sample;
 
-.bad_format
-	lda #>.bad_audio_format_msg
-	ldx #<.bad_audio_format_msg
-	jmp printstring_raw
-
-.skip_chunk
-    ldz #3
-    lda [sound_file_target],z
-    jsr .add_fastRAM_base ; skip chunk data (TODO check full size?)
-    lda #4
-    jsr .add_fastRAM_base ; skip chunk length (4 bytes)
-    jmp .check_chunk
-
-.ssnd_chunk
-    ; is the sample too big?
-    ldz #1
-    lda [sound_file_target],z
-    bne .bad_format
-
-    ; save chunk size for later
-    inz
--   lda [sound_file_target],z
-    pha
-    inz
-    cpz #4
-    bne -
-
-    lda #12
-    jsr .add_fastRAM_base ; skip until sample data (assuming no comment)
+.play_sample
     ; stop playback while loading new sample data
     lda #$00
     sta $d720
-    ; load sample address into base and current address
-    lda sound_file_target 
+    ; store sample start address in base and current address
+    lda sample_start_address
     sta $d721 ; base 
     sta $d72a ; current
-    lda sound_file_target + 1
+    lda sample_start_address + 1
     sta $d722
     sta $d72b
-    lda sound_file_target + 2
+    lda sample_start_address + 2
     sta $d723
     sta $d72c
-    ; calculate end point by adding saved chunk size to sample start
-    ; todo: adjust for comment and chunk header (-8)
-    clc
-    pla
-    adc $d721
+    ; store sample stop address
+    lda sample_stop_address
     sta $d727
-    pla
-    adc $d722
+    lda sample_stop_address + 1
     sta $d728
 
     ; volume
-	lda sound_arg_volume
+    lda sound_arg_volume
     sta $d729
     sta $d71c ; mirror channel for stereo
 
     ; frequency (assuming CPU running at 40 MHz)
-    ; x = (f * 40)/100 = (f/10) << 2
-    lda .sample_rate_big_endian + 1 ; note big endian
-    sta dividend
-    lda .sample_rate_big_endian
-    sta dividend + 1
-    lda #10
-    sta divisor
-    lda #$00
-    sta divisor + 1
-    jsr divide16
-    clc
-    rol dividend
-    rol dividend + 1
-    rol dividend
-    rol dividend + 1
-    lda dividend
+    lda sample_frequency
     sta $d724
-    lda dividend + 1
+    lda sample_frequency + 1
     sta $d725
     lda #$00
     sta $d726
@@ -470,25 +357,9 @@ sound_effect
 
     rts
 
-.init_fastRAM_base
-    ; init sound file address pointer
-	lda #0
-	sta sound_file_target
-	sta sound_file_target + 1
-	sta sound_file_target + 3
-	lda #$04
-	sta sound_file_target + 2
-	rts
-
-.add_fastRAM_base
-    ; add (a) to sound file address pointer
-    clc
-    adc sound_file_target
-    sta sound_file_target
-    lda sound_file_target + 1
-    adc #$00
-    sta sound_file_target + 1
-    rts
+!ifdef SOUND_AIFF_ENABLED {
+!source "sound-aiff.asm"
+}
 
 .copy_effect_to_fastram
     ; copy effect .current_effect to fastRAM so it can be played
