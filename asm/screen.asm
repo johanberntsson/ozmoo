3c0c706bb7dfddc174603db690cbe73371732b75
Fredrik Ramsberg <fredrik.ramsberg@gmail.com>
2023-07-19 12:08:27 +0200

    Fixed printing bug for C128 80 col as well.

--- asm/screen.asm
+++ asm/screen.asm
@@ -626,7 +626,7 @@ print_line_from_buffer
 
 	ldx first_buffered_column
 -   cpx last_break_char_buffer_pos
-	beq .done_print_80
+	bcs .done_print_80
 	lda print_buffer,x
 	jsr convert_petscii_to_screencode
 	ora print_buffer2,x
