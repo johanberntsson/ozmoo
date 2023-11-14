aec08417c2b02ebbaf683ea95dcda24b859c6d34
Fredrik Ramsberg <fredrik.ramsberg@gmail.com>
2023-11-14 22:26:51 +0100

    Added test build script

---
+++ tests/test_builds.rb
@@ -0,0 +1,28 @@
+# $is_windows = (ENV['OS'] == 'Windows_NT')
+
+require 'fileutils'
+
+def test_build(storyfile, target, mode, params, outfiles)
+	targetstr = target.length > 0 ? " -t:#{target}" : ''
+	modestr = mode.length > 0 ? " -#{mode}" : ''
+	paramsstr = params.length > 0 ? " #{params}" : ''
+	cmd = "ruby ../make.rb #{storyfile}#{targetstr}#{modestr}#{paramsstr}"
+	puts "BUILD: #{cmd}"
+	out = `#{cmd}`
+	problem = false
+	problem = true if out =~ /^ERROR:/
+	if problem then
+		puts out
+		print "-----------------------------------------\n"
+	else
+		outfiles.keys.each { |filename|
+			File.delete(filename)
+		}
+	end
+	
+end
+
+test_build('../infocom/minizork.z3', 'c64', 'P', '', {'c64_minizork.d64' => 1})
+test_build('../infocom/minizork.z3', 'plus4', 'S1', '', {'plus4_minizork.d64' => 1})
+test_build('../infocom/minizork.z3', 'c128', '', '', {'c128_minizork.d71' => 1})
+test_build('../infocom/minizork.z3', 'mega65', '', '', {'mega65_minizork.d81' => 1})
