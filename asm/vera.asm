d3ede5ee28972b052fe402923402572ee0ebf67c
Johan Berntsson <johan.mayumi@gmail.com>
2023-11-30 21:40:29 +0900

    scrolling now work in screenkernal for x16

--- asm/vera.asm
+++ asm/vera.asm
@@ -1,9 +1,22 @@
 ; Support routines for VERA access on the X16
+; https://github.com/X16Community/x16-docs/blob/master/VERA%20Programmer%27s%20Reference.md
+;
+; Standard text: video memory starts at $b000,
+; and each character is specified by a (screencode,colour) tuple.
+;
+; Each line has up to 128 characters even if only 80 is shown as
+; default, so the first character in the second line is at $b100
+; $b000, $b002, $b004 ...
+; $b100, $b102, $b104 ...
+; ...
+;
+
 
 VERA_addr_low		= $9f20
 VERA_addr_high		= $9f21
 VERA_addr_bank		= $9f22
 VERA_data0			= $9f23
+VERA_data1			= $9f24
 VERA_ctrl			= $9f25
 VERA_dc_video		= $9f29
 VERA_dc_hscale		= $9f2a
